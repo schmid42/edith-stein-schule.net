c075e8282d8095ae6aecbe130e646773f4547d66
peddn <public@peddn.net>
2022-03-28 08:16:37 +0200

    sections

--- backend/content/models.py
+++ backend/content/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, FieldRowPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 
-class ContentPage(Page):
+
+class ContentPageSections(Orderable):
+    """TODO"""
+    page    = ParentalKey("content.ContentPage", related_name="content_sections")
+    heading = models.CharField(blank=True, max_length=256)
+    body    = RichTextField(blank=True)
 
     cover = models.ForeignKey(
         'wagtailimages.Image',
@@ -15,17 +22,32 @@ class ContentPage(Page):
         related_name='+'
     )
 
+
+    panels = [
+        FieldPanel('heading', heading="Überschrift"),
+        ImageChooserPanel('cover', heading="Bild zu diesem Abschnitt"),
+        FieldPanel('body', heading="Inhalt")
+    ]
+
+
+
+
+class ContentPage(Page):
+
+
     show_heading    = models.BooleanField(default=True)
     heading         = models.CharField(blank=True, max_length=256)
-    show_subheading = models.BooleanField(default=True)
-    subheading      = models.CharField(blank=True, max_length=256)
+    show_body    = models.BooleanField(default=True)
     body            = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('show_heading', classname="full", heading="Seitentitel anzeigen"),
-        FieldPanel('heading', classname="full", heading="Seitentitel"),
-        FieldPanel('show_subheading', classname="full", heading="Untertitel anzeigen"),
-        FieldPanel('subheading', classname="full", heading="Untertitiel"),
-        ImageChooserPanel('cover', classname="full", heading="Bild zu dieser Seite"),
-        FieldPanel('body', classname="full", heading="Inhalt")
+        MultiFieldPanel(
+            [
+                FieldPanel('heading', heading="Seitenüberschrift"),
+                FieldPanel('show_heading', heading="Seitenüberschrift anzeigen"),
+                FieldPanel('body', heading="Inhalt"),               
+                FieldPanel('show_body', heading="Seiteninhalt anzeigen")
+            ]
+        ),        
+        InlinePanel('content_sections', heading="Abschnitte")
     ]
--- frontend/content/js/main.js
+++ frontend/content/js/main.js
@@ -1,3 +1,47 @@
 require('../scss/main.scss');
 
 console.log("Hello from content!");
+
+const links = document.querySelectorAll(".page-header ul a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+  
+      // Add a click event on each of them
+      $navbarBurgers.forEach( el => {
+        el.addEventListener('click', () => {
+  
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target;
+          const $target = document.getElementById(target);
+  
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+  
+        });
+      });
+    }
+  
+  });
