c0454f374cf437c0d683bb57be5a885472eb6ff5
peddn <public@peddn.net>
2022-07-18 19:21:05 +0200

    added template for postgresql

--- backend/main/settings/dev.py
+++ backend/main/settings/dev.py
@@ -27,6 +27,17 @@ DATABASES = {
     }
 }
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'mydatabase',
+#        'USER': 'mydatabaseuser',
+#        'PASSWORD': 'mypassword',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
+#    }
+#}
+
 try:
     from .local import *
 except ImportError:
